1b6ab5f3a1000de62e63200abf796023aea38c41
cur10sDEV <specsx@protonmail.com>
2023-12-24 10:33:37 +0530

    added question form with validations using zod and components of shandcn ui

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,5 +1,14 @@
+import Question from "@/components/forms/Question";
+
 type Props = {};
 const AskQuestion = (props: Props) => {
-  return <div>AskQuestion</div>;
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a public question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 };
 export default AskQuestion;
