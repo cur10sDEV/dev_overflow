5f7fa12eb675eda888a710f4df84a54548439ae8
cur10sDEV <specsx@protonmail.com>
2024-01-09 12:54:49 +0530

    add tag details page and minor bug fixes

---
+++ app/(root)/tags/[id]/page.tsx
@@ -0,0 +1,67 @@
+import QuestionCard from "@/components/cards/QuestionCard";
+import NoResult from "@/components/shared/NoResult";
+import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
+import { getQuestionsByTagId } from "@/lib/actions/tag.action";
+
+interface Props {
+  params: {
+    id: string;
+  };
+  searchParams: {
+    q: string;
+  };
+}
+
+const page = async ({ params, searchParams }: Props) => {
+  const result = await getQuestionsByTagId({
+    tagId: params.id,
+    page: 1,
+    pageSize: 10,
+    searchQuery: searchParams.q,
+  });
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">{result.tagTitle}</h1>
+
+      <div className="mt-11 w-full">
+        <LocalSearchBar
+          route="/"
+          iconPosition="left"
+          icon="/assets/icons/search.svg"
+          placeholder="Search tag questions..."
+          otherClasses="flex-1"
+        />
+      </div>
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result.questions.length > 0 ? (
+          result.questions.map((item: any) => {
+            return (
+              <QuestionCard
+                key={item._id}
+                _id={item._id}
+                title={item.title}
+                tags={item.tags}
+                author={item.author}
+                upvotes={item.upvotes}
+                views={item.views}
+                answers={item.answers}
+                createdAt={item.createdAt}
+              />
+            );
+          })
+        ) : (
+          <NoResult
+            title="There's no tag questions to show"
+            description="Be the first to break the silence! 🚀 Ask a question and kickstart the
+        discussion. Our query could be the next big thing others learn from. Get
+        involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
+    </>
+  );
+};
+export default page;
--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -1,9 +1,15 @@
 "use server";
 
-import Tag from "@/database/tag.model";
+import Question from "@/database/question.model";
+import Tag, { TagInterface } from "@/database/tag.model";
 import User from "@/database/user.model";
+import { FilterQuery } from "mongoose";
 import { connectDB } from "../mongoose";
-import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import {
+  GetAllTagsParams,
+  GetQuestionsByTagIdParams,
+  GetTopInteractedTagsParams,
+} from "./shared.types";
 
 export const getTopInteractedTags = async (
   params: GetTopInteractedTagsParams
@@ -55,6 +61,47 @@ export const getAllTags = async (params: GetAllTagsParams) => {
     const tags = await Tag.find({});
 
     return { tags };
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const getQuestionsByTagId = async (
+  params: GetQuestionsByTagIdParams
+) => {
+  try {
+    connectDB();
+
+    // const { tagId, searchQuery = "", page = 1, pageSize = 10 } = params;
+    const { tagId, searchQuery = "" } = params;
+
+    const tagFilter: FilterQuery<TagInterface> = { _id: tagId };
+
+    const tag = await Tag.findOne(tagFilter).populate({
+      path: "questions",
+      model: Question,
+      match: searchQuery
+        ? { title: { $regex: searchQuery, $options: "i" } }
+        : {},
+      options: {
+        sort: { createdAt: -1 },
+      },
+      populate: [
+        { path: "tags", model: Tag, select: ["_id", "name"] },
+        {
+          path: "author",
+          model: User,
+          select: ["_id", "name", "clerkId", "picture"],
+        },
+      ],
+    });
+
+    if (!tag) {
+      throw new Error("Tag not found");
+    }
+
+    return { tagTitle: tag.name, questions: tag.questions };
   } catch (error) {
     console.error(error);
     throw error;
