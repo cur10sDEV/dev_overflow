ba23b0bd87692c6f105636b60bd80410f35e970d
cur10sDEV <specsx@protonmail.com>
2024-01-14 12:31:01 +0530

    add profile edit page

---
+++ app/(root)/profile/edit/page.tsx
@@ -0,0 +1,25 @@
+import Profile from "@/components/forms/Profile";
+import { getUserById } from "@/lib/actions/user.action";
+import { ParamsProps } from "@/types";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const page = async ({ params }: ParamsProps) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("sign-in");
+  }
+
+  const mongoUser = await getUserById({ userId });
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark400_light900">Edit Question</h1>
+      <div className="mt-9">
+        <Profile clerkId={userId} user={JSON.stringify(mongoUser)} />
+      </div>
+    </>
+  );
+};
+export default page;
