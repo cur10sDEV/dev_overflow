7fbb352c2057d4f715cf52ef5af25f3fa7648b17
cur10sDEV <specsx@protonmail.com>
2024-02-05 19:38:19 +0530

    add generate ai answer

---
+++ app/api/chatgpt/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+
+export const POST = async (req: Request) => {
+  const { question } = await req.json();
+
+  try {
+    const res = await fetch(`https://api.openai.com/v1/chat/completions`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content:
+              "You're a knowledgeable assistant that provides quality information.",
+          },
+          { role: "user", content: `Tell me ${question}` },
+        ],
+      }),
+    });
+
+    const resData = await res.json();
+    console.log(resData);
+    const reply = resData.choices[0].message.content;
+    return NextResponse.json({ reply });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message });
+  }
+};
